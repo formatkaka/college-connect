b9ace2a92d12f9b3c6bc2a1cfa41fb5b4fffc35e
yashm28 <modiyash28@yahoo.com>
2015-12-30 11:05:21 +0530

    Initial

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 from __init__ import *
+import os
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://siddhant:collegeconnect@localhost:5432/mydata'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 api = Api(app)
 app.config['SECRET_KEY'] = 's4df65a4g6aksdh<>?:.[],-.906^&#hcsddsf]!@#%^^^&*d[bjhv]'
