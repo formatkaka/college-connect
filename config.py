138a5a6a15196ec2450e1ad47f983c40e0c9ca7c
supertramp1995 <sid.siddhant.loya@gmail.com>
2016-01-04 22:58:52 +0530

    Bug fix

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ from flask_mail import Mail
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://siddhant:collegeconnect@localhost:5432/db2'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://siddhant:collegeconnect@localhost:5432/db2'
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://pvokrejooerxqb:OK2EVuhUXYxoVtIdO_rfCz7Ev6@ec2-107-22-170-249.compute-1.amazonaws.com:5432/d8bgfga3qa0vu7'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://pvokrejooerxqb:OK2EVuhUXYxoVtIdO_rfCz7Ev6@ec2-107-22-170-249.compute-1.amazonaws.com:5432/d8bgfga3qa0vu7'
 db = SQLAlchemy(app)
 api = Api(app)
 mail = Mail(app)
