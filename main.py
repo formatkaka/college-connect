99cb2de6d449ce31bea29352b66f0db1f03e18ad
supertramp1995 <sid.siddhant.loya@gmail.com>
2016-01-15 17:22:34 +0530

    -

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ class UserRegistration(Resource):
                           recipients=recieve)
 
             msg.body = "please click on the link {0}".format(link)
-            # mail.send(msg)
+            mail.send(msg)
 
             return result.data
 
@@ -328,7 +328,7 @@ api.add_resource(Testing1, '/api/test')
 
 if __name__ == "__main__":
     # db.create_all()
-    # manager.run()
-    # port = int(os.environ.get('PORT', 5432))
-    # app.run(host='0.0.0.0', port=port, debug=True)
-    app.run(port=8080,debug=True)
+    manager.run()
+    port = int(os.environ.get('PORT', 5432))
+    app.run(host='0.0.0.0', port=port, debug=True)
+    # app.run(port=8080,debug=True)
