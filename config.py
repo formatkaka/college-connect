c9af105f2bf8e888168e4206572d3d174055e953
supertramp1995 <sid.siddhant.loya@gmail.com>
2016-01-17 11:43:36 +0530

    config error

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/databse'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://pvokrejooerxqb:OK2EVuhUXYxoVtIdO_rfCz7Ev6@ec2-107-22-170-249.compute-1.amazonaws.com:5432/d8bgfga3qa0vu7'
-db = SQLAlchemy(app)/
+db = SQLAlchemy(app)
 api = Api(app)
 mail = Mail(app)
 app.config['SECRET_KEY'] = 's4df65a4g6aksdh<>?:.[],-.906^&#hcsddsf]!@#%^^^&*d[bjhv]'
