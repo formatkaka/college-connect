c3eca0779102c05c43299733129031c27a9be93d
yashm28 <modiyash28@yahoo.com>
2016-01-23 22:06:53 +0530

    bugfix

--- scrap.py
+++ scrap.py
@@ -8,7 +8,7 @@ os.environ['https_proxy']=''
 os.environ['no_proxy']='svnit.ac.in'
 
 import urllib2, requests
-from urllib2 import HTTPError
+from requests.exceptions
 
 class Scrap:
 	url = 'http://svnit.ac.in:80/'
