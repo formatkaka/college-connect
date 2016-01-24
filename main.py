b86b4559f9ccb8cd1eb5e21e0df22e9efe18d54a
supertramp1995 <sid.siddhant.loya@gmail.com>
2016-01-24 19:57:16 +0530

    -

--- main.py
+++ main.py
@@ -431,8 +431,8 @@ api.add_resource(AddRemoveAdmin, '/api/admin')
 if __name__ == "__main__":
     manager.run()
     # db.create_all()
-    port = int(os.environ.get('PORT', 5432))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    # port = int(os.environ.get('PORT', 5432))
+    # app.run(host='0.0.0.0', port=port, debug=True)
     # app.run(port=8080,debug=True)
 
     # TODO - 1. server_id for event and clubs
