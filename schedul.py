b0ec4538a4af3d625e25a67b38fce04cdefbc61d
supertramp1995 <sid.siddhant.loya@gmail.com>
2016-01-27 10:09:13 +0530

    -

--- schedul.py
+++ schedul.py
@@ -1,14 +1,37 @@
-import sched
+from datetime import datetime
 import time
+import os
 
-scheduler = sched.scheduler(time.time, time.sleep)
+from config import mail
+from flask_mail import Message
+from apscheduler.schedulers.background import BackgroundScheduler
+import logging
+logging.basicConfig()
 
-def print_event(name):
-    print 'EVENT:', time.time(), name
+def tick():
+    msg = Message(subject="Thank You for Registration.Confirmation Link.Click Below.",
+                  sender="college.connect01@gmail.com",
+                  recipients=["college.connect28@gmail.com"])
 
-print 'START:', time.time()
-a = input('')
-scheduler.enter(a, 1, print_event, ('first',))
-scheduler.enter(10, 1, print_event, ('second',))
+    msg.body = "please click on the link "
+    mail.send(msg)
+    print "sent"
 
-scheduler.run()
+scheduler = BackgroundScheduler()
+scheduler.add_job(tick, 'interval', seconds=10)
+scheduler.start()
+
+# if __name__ == '__main__':
+#     scheduler = BackgroundScheduler()
+#     scheduler.add_job(tick, 'interval', seconds=10)
+#     scheduler.start()
+#     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+#     # scheduler.remove_all_jobs()
+#     # print "done"
+#     try:
+#         # This is here to simulate application activity (which keeps the main thread alive).
+#         while True:
+#             time.sleep(2)
+#     except (KeyboardInterrupt, SystemExit):
+#         # Not strictly necessary if daemonic mode is enabled but should be done if possible
+#         scheduler.shutdown()
