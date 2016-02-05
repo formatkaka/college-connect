33c4fb665f84ce89c72c2cf4696ca318733a46bb
supertramp1995 <sid.siddhant.loya@gmail.com>
2016-02-05 22:13:21 +0530

    Email verification changes

--- main.py
+++ main.py
@@ -575,9 +575,9 @@ if __name__ == "__main__":
 	# thread.start()
 	# manager.run()
 	db.create_all()
-	# port = int(os.environ.get('PORT', 8080))
-	# app.run(host='0.0.0.0', port=port, debug=True)
-	app.run(port=8080, debug=True)
+	port = int(os.environ.get('PORT', 8080))
+	app.run(host='0.0.0.0', port=port, debug=True)
+	# app.run(port=8080, debug=True)
 	# except KeyboardInterrupt:
 	# 	raise KeyError('j')
 
