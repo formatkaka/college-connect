9026e6845c2035e540895c5d3597447c302883d8
supertramp1995 <sid.siddhant.loya@gmail.com>
2016-02-09 11:56:39 +0530

    email sendgrid fix

--- config.py
+++ config.py
@@ -22,11 +22,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['SQLALCHEMY_MAX_OVERFLOW']=0
 app.config['SQLALCHEMY_POOL_SIZE'] = 2
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = 'college.connect01@gmail.com'
+app.config['MAIL_USERNAME'] = 'collegeconnect'
 app.config['MAIL_PASSWORD'] = 'collegeconnect1234'
 # app.config['MAIL_USERNAME'] = 'college.connect28@gmail.com'
 # app.config['MAIL_PASSWORD'] = 'collegeconnect1234'
