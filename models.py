f0349dd36ed47446ba601761587406c6c5a42f17
supertramp1995 <sid.siddhant.loya@gmail.com>
2016-02-16 13:01:35 +0530

    phone no.

--- models.py
+++ models.py
@@ -599,7 +599,7 @@ def get_admin_info(club):
     admins = []
     if club.id < 150:
         for admin in club.adminsList:
-            a = Admins(admin.fullName, admin.mobNo, admin.emailId)
+            a = Admins(admin.fullName + " " + str(admin.mobNo), 0, admin.emailId)
             admins.append(a)
         return admins
     else:
